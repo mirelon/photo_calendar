088171b454900ae968cf31c6fb0ca908c8ea0842
mirelon <miso@github.ksp.sk>
2013-12-16 23:04:16 +0100

    initial commit

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+PhotoCalendar::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PhotoCalendar::Application.config.secret_token = 'a9e1b46610602e3d37c9e46864945c112d2133bbc99fc2c035fddf21dee59aea54e4b24247aad5ef170a3cc059e7c8095844fd90c473027ba83c4caf922e99bd'
