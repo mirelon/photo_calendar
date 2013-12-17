258b002fb088e220e7fb88ce4d3dafd50e1e288a
mirelon <miso@github.ksp.sk>
2013-12-17 20:56:18 +0100

    Added some variables, centered month name

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -6,30 +6,37 @@ class CalendarsController < ApplicationController
   end
 
   def show
-    width = 100
-    height = 80
+    cell_width = 100
+    cell_height = 80
+    page_size = "A3"
+
   	@people = Calendar.find(params[:id]).people
     @year = (params[:year] || Date.current.year + 1).to_i
-    pdf = Prawn::Document.new(page_size: "A3", page_layout: :portrait)
+    pdf = Prawn::Document.new(page_size: page_size, page_layout: :portrait)
+    page_width = pdf.bounds.width
+    page_height = pdf.bounds.height
     start_date = Date.new(@year)
     end_date = start_date.end_of_year
     (start_date .. end_date).each do |date|
       if date.day == 1
-        pdf.draw_text Date::MONTHNAMES[date.month], at: [300, 530]
+        pdf.text_box Date::MONTHNAMES[date.month],
+          at: [page_width/2-cell_width/2, 550],
+          width: cell_width,
+          align: :center
         (0..6).each do |weekday|
-          pdf.draw_text Date::DAYNAMES[(weekday + 1) % 7], at: [weekday * width, 515] 
+          pdf.draw_text Date::DAYNAMES[(weekday + 1) % 7], at: [weekday * cell_width, 515] 
         end
       end
-      x1 = (date.wday - date.previous_monday.wday)%7 * width
-      y1 = 500 - date.all_sundays_in_month.select{|i| i<date.day}.size * height
-      pdf.rectangle [x1, y1], width, height
+      x1 = (date.wday - date.previous_monday.wday)%7 * cell_width
+      y1 = 500 - date.all_sundays_in_month.select{|i| i<date.day}.size * cell_height
+      pdf.rectangle [x1, y1], cell_width, cell_height
       pdf.stroke
-      pdf.rectangle [x1, y1], width, 20
+      pdf.rectangle [x1, y1], cell_width, 20
       pdf.stroke
       pdf.draw_text date.day.to_s, at: [x1 + 7, y1 - 15], style: :bold
       @people.each do |person|
         if person.day.day == date.day and person.day.month == date.month
-          pdf.image person.photo, at: [x1 + 10, y1 - 30], fit: [width-50, height-40]
+          pdf.image person.photo, at: [x1 + 10, y1 - 30], fit: [cell_width-50, cell_height-40]
         end
       end
       if date.tomorrow.day == 1 and not date.tomorrow.january?
