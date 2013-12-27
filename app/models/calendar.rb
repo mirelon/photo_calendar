17150acaed3bd23235bd28369f3a1495cf898b48
mirelon <miso@github.ksp.sk>
2013-12-27 11:25:52 +0100

    Layout changes

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -28,7 +28,7 @@ class Calendar < ActiveRecord::Base
     page_width = pdf.bounds.width
     page_height = pdf.bounds.height
     cell_width = pdf.bounds.width / 7
-    cell_height = 80
+    cell_height = 100
     cell_header_height = 20
     subtitle_box_width = cell_width
     subtitle_box_height = 15
@@ -37,14 +37,15 @@ class Calendar < ActiveRecord::Base
     end_date = start_date.end_of_year
     (start_date .. end_date).each do |date|
       if date.day == 1
+        top_grid_y = (date.all_sundays_in_month.size + 1) * cell_height
         month_filename = "10" + date.month.to_s.rjust(2,'0') + ".JPG"
         pdf.image "public/uploads/#{id}/#{month_filename}",
-          at: [-pdf.page.margins[:left], page_height+pdf.page.margins[:top]+(date.month == 12 ? 0 : 100)],
-          width: page_width+pdf.page.margins[:left]+pdf.page.margins[:right]
+          height: page_height - top_grid_y - 80,
+          position: :center
         pdf.fill_color "cc0000"
         pdf.text_box I18n.t('date.month_names', locale: :sk)[date.month] + " #{year}",
-          at: [0, 570],
-          width: 4*cell_width,
+          at: [0, top_grid_y + 70],
+          width: 7*cell_width,
           size: 36
         (0..6).each do |weekday|
           if weekday==6
@@ -53,14 +54,14 @@ class Calendar < ActiveRecord::Base
             pdf.fill_color "000000"
           end
           pdf.text_box I18n.t('date.day_names', locale: :sk)[(weekday + 1) % 7],
-            at: [weekday * cell_width, 520],
+            at: [weekday * cell_width, top_grid_y + 20],
             width: cell_width,
             align: :center
           pdf.fill_color "000000"
         end
       end
       x1 = (date.wday - date.previous_monday.wday)%7 * cell_width
-      y1 = 500 - date.all_sundays_in_month.select{|i| i<date.day}.size * cell_height
+      y1 = (date.all_sundays_in_month.select{|i| i>=date.day}.size + 1) * cell_height
       pdf.rectangle [x1, y1], cell_width, cell_height
       pdf.stroke
       pdf.rectangle [x1, y1], cell_width, cell_header_height
